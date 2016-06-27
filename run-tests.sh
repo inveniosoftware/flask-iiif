db7a2e9ee4c3e0d3aa09e2d21d451540df2abd00
Tibor Simko <tibor.simko@cern.ch>
2016-06-27 20:31:45 +0200

    tests: pep257 to pydocstyle
    Signed-off-by: Tibor Simko <tibor.simko@cern.ch>

--- run-tests.sh
+++ run-tests.sh
@@ -7,7 +7,7 @@
 # it under the terms of the Revised BSD License; see LICENSE file for
 # more details.
 
-pep257 flask_iiif
+pydocstyle flask_iiif
 check-manifest --ignore ".travis-*" --ignore docs/_themes
 sphinx-build -qnNW docs docs/_build/html
 python setup.py test
